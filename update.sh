0ae0b0384af13d9c0c8f643d6dc44207fc61bdda
Kyle Mulleady <kyle@windwardapps.com>
2020-03-29 17:28:50 -0700

    automation

--- update.sh
+++ update.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env bash
 
-cd ../COVID-19
+pushd ../COVID-19
 git pull
-cd ../91-DIVOC/pages/covid-visualization
+popd
+cd pages/covid-visualization
 python3 processData.py
 python3 processUrls.py
