4c970a1f78008b488d467c2e0290be9fdf212fff
Kyle Mulleady <kyle@windwardapps.com>
2020-03-29 18:03:19 -0700

    logging

--- update.sh
+++ update.sh
@@ -1,8 +1,12 @@
 #! /usr/bin/env bash
 
+echo "[$(date "+%Y-%m-%dT%H:%M:%S")] start"
+
 pushd ../COVID-19
 git pull
 popd
 cd pages/covid-visualization
 python3 processData.py
 python3 processUrls.py
+
+echo "[$(date "+%Y-%m-%dT%H:%M:%S")] end"
